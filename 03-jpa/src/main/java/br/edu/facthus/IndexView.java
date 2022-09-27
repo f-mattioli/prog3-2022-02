87136616cb8acb82b523b5df534d7acdbb933c43
Fernando Mattioli <fernando.mattioli@facthus.edu.br>
2022-09-26 21:29:05 -0300

    Completa o exemplo JPA

--- 03-jpa/src/main/java/br/edu/facthus/IndexView.java
+++ 03-jpa/src/main/java/br/edu/facthus/IndexView.java
@@ -1,16 +1,32 @@
 package br.edu.facthus;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.edu.facthus.entity.Contato;
+import br.edu.facthus.service.ContatosService;
 
 @Named
+@RequestScoped
 public class IndexView {
+	
+	@Inject
+	private ContatosService contatosService;
 
 	private Contato contato = new Contato();
+	
+	private List<Contato> contatos = new ArrayList<>();
 
 	public void cadastra() {
-		System.out.println("Em construção...");
+		contatosService.cadastra(contato);
+	}
+
+	public void lista() {
+		contatos = contatosService.lista();
 	}
 
 	public Contato getContato() {
@@ -19,6 +35,14 @@ public class IndexView {
 
 	public void setContato(Contato contato) {
 		this.contato = contato;
+	}
+
+	public List<Contato> getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(List<Contato> contatos) {
+		this.contatos = contatos;
 	}
 	
 }
--- 03-jpa/src/main/java/br/edu/facthus/service/ContatosService.java
+++ 03-jpa/src/main/java/br/edu/facthus/service/ContatosService.java
@@ -1,8 +1,12 @@
 package br.edu.facthus.service;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import br.edu.facthus.entity.Contato;
 
 @Stateless
 public class ContatosService {
@@ -10,6 +14,15 @@ public class ContatosService {
     @PersistenceContext
     private EntityManager entityManager;
 
+    public void cadastra(Contato contato) {
+        entityManager.persist(contato);
+        System.out.println("Contato inserido com sucesso!");
+    }
 
+    public List<Contato> lista() {
+        return entityManager
+            .createQuery("SELECT c FROM Contato c", Contato.class)
+            .getResultList();
+    }
     
 }
